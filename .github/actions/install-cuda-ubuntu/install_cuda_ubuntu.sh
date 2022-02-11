9325d6cafac5298c7623902fe4c3a432f6bfd9c3
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-02-11 09:15:42 +0900

    Update install_cuda_ubuntu.sh

--- .github/actions/install-cuda-ubuntu/install_cuda_ubuntu.sh
+++ .github/actions/install-cuda-ubuntu/install_cuda_ubuntu.sh
@@ -172,7 +172,9 @@ fi
 
 sudo tar -hxvf cudnn.tgz -C "$CUDA_PATH"
 if [[ "$CUDA_VERSION_MAJOR_MINOR" == *"11.6"* ]]; then
+   echo "Listing base Cuda Path"
    ls "$CUDA_PATH"
+   echo "Listing cuda path with archive"
    ls "$CUDA_PATH/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive/"
    sudo cp -rf  "$CUDA_PATH/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive/*" "$CUDA_PATH"
 fi
