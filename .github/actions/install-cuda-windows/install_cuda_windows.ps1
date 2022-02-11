ddaa5132206a744d385308bf7c4302fb3ab958fa
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-02-11 23:16:24 +0900

    Update install_cuda_windows.ps1

--- .github/actions/install-cuda-windows/install_cuda_windows.ps1
+++ .github/actions/install-cuda-windows/install_cuda_windows.ps1
@@ -7,7 +7,7 @@ $CUDA_KNOWN_URLS = @{
       "11.0.167" = "http://developer.download.nvidia.com/compute/cuda/11.0.3/network_installers/cuda_11.0.3_win10_network.exe";
       "11.2.1_461" = "https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_461.09_win10_network.exe";
       "11.4.1" = "https://developer.download.nvidia.com/compute/cuda/11.4.1/network_installers/cuda_11.4.1_win10_network.exe";
-      "11.6.0" = "https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_511.23_windows.exe";
+      "11.6.0" = "https://developer.download.nvidia.com/compute/cuda/11.6.0/network_installers/cuda_11.6.0_windows_network.exe";
 
 }
 
@@ -15,7 +15,7 @@ $CUDA_FILE_NAMES = @{
     "11.0.167" = "cuda_11.0.3_win10_network.exe";
     "11.2.1" = "cuda_11.2.1_461.09_win10_network.exe";
     "11.4.1" = "cuda_11.4.1_win10_network.exe";
-    "11.6.0" = "cuda_11.6.0_511.23_windows.exe";
+    "11.6.0" = "cuda_11.6.0_windows_network.exe";
 }
 
 # https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.4/11.0_20200923/cudnn-11.0-windows-x64-v8.0.4.30.zip
