aa38b7a54cc627aed56634251f991826a1e81a72
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2023-06-22 17:39:23 +0900

    Update install_cuda_windows.ps1

--- .github/actions/install-cuda-windows/install_cuda_windows.ps1
+++ .github/actions/install-cuda-windows/install_cuda_windows.ps1
@@ -8,6 +8,8 @@ $CUDA_KNOWN_URLS = @{
       "11.2.1_461" = "https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_461.09_win10_network.exe";
       "11.4.1" = "https://developer.download.nvidia.com/compute/cuda/11.4.1/network_installers/cuda_11.4.1_win10_network.exe";
       "11.6.0" = "https://developer.download.nvidia.com/compute/cuda/11.6.0/network_installers/cuda_11.6.0_windows_network.exe";
+      "11.8.0" = "https://developer.download.nvidia.com/compute/cuda/11.8.0/network_installers/cuda_11.8.0_windows_network.exe";
+      "12.1" = "https://developer.download.nvidia.com/compute/cuda/12.1.1/network_installers/cuda_12.1.1_windows_network.exe";
 
 }
 
@@ -16,6 +18,8 @@ $CUDA_FILE_NAMES = @{
     "11.2.1" = "cuda_11.2.1_461.09_win10_network.exe";
     "11.4.1" = "cuda_11.4.1_win10_network.exe";
     "11.6.0" = "cuda_11.6.0_windows_network.exe";
+    "11.8.0" = "cuda_11.8.0_windows_network.exe";
+    "12.1" = "cuda_12.1.1_windows_network.exe";
 }
 
 # https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.4/11.0_20200923/cudnn-11.0-windows-x64-v8.0.4.30.zip
@@ -25,7 +29,8 @@ $CUDNN_KNOWN_URLS = @{
       "11.2.1" = "https://developer.download.nvidia.com/compute/redist/cudnn/v8.1.0/cudnn-11.2-windows-x64-v8.1.0.77.zip";
       "11.4.1" = "https://developer.download.nvidia.com/compute/redist/cudnn/v8.2.2/cudnn-11.4-windows-x64-v8.2.2.26.zip";
       "11.6.0" = "https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip";
-     
+      "11.8.0" = "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-windows-x86_64-8.6.0.163_cuda11-archive.zip";
+      "12.1" = "https://developer.download.nvidia.com/compute/redist/cudnn/v8.9.2/local_installers/12.x/cudnn-windows-x86_64-8.9.2.26_cuda12-archive.zip";
 }
 
 # cuda_runtime.h is in nvcc <= 10.2, but cudart >= 11.0
